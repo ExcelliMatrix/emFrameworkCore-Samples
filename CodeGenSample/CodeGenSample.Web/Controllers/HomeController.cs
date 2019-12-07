3443bebbbe43ee83e84dc59e5de17c03eb9effe2
Leland R. Johnson <Leland.Johnson@ExcelliMatrix.com>
2019-12-07 14:51:42 -0700

    Intermin check-in of in process work.

--- CodeGenSample/CodeGenSample.Web/Controllers/HomeController.cs
+++ CodeGenSample/CodeGenSample.Web/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace CodeGenSample.Web.Controllers
       return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult Automobile()
     {
       return View();
     }
---
+++ CodeGenSample/README.md
@@ -0,0 +1,2 @@
+# emFrameworkCore-Samples / CodeGenSample
+Examples of using T4 templeates with the emFrameworkCore.CodeGen
